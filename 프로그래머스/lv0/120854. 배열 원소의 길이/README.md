6b9fb1bedbbbf534a42cf7812c8c8f0372f15c1b
rhdqngusanr <59686942+rhdqngusanr@users.noreply.github.com>
2022-12-07 15:48:15 +0900

    [level 0] Title: 배열 원소의 길이, Time: 8.37 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/README.md
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 원소의 길이 - 120854 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120854) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120854?language=kotlin) 
 
 ### 성능 요약
 
-메모리: 64.2 MB, 시간: 28.82 ms
+메모리: 61.6 MB, 시간: 8.37 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.kt
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.kt
@@ -1,7 +1,4 @@
 class Solution {
-    fun solution(strlist: Array<String>): IntArray {
-        //var answer: IntArray = intArrayOf()
-        //strlist.map{it -> strlist.chunked(1).size}.toIntArray()
-        return strlist.toList().map{it -> it.chunked(1).size}.toIntArray()
-    }
+    fun solution(strlist: Array<String>) = strlist.map {it.length }.toIntArray()
+ 
 }
