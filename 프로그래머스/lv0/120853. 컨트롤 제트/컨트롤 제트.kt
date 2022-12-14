b8b330e73c201c2985e8986e9dbfd428b191b603
rhdqngusanr <59686942+rhdqngusanr@users.noreply.github.com>
2022-12-14 15:49:54 +0900

    [level 0] Title: 컨트롤 제트, Time: 7.19 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.kt
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.kt
@@ -5,12 +5,7 @@ class Solution {
         var array = s.split(" ")
         for(i in array.indices){
             if(array[i]== "Z"){
-                if(array[i-1].toInt() >= 0){
-                    answer -= array[i-1].toInt()
-                }else{
-                    answer += abs(array[i-1].toInt())
-                }
-                
+                answer -= array[i-1].toInt()  
             }else{
                 answer+= array[i].toInt()
             }
