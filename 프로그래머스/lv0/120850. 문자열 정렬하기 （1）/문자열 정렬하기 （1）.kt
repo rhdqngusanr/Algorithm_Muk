334914194786735596c35bc054c7b62e6d66f6b7
rhdqngusanr <59686942+rhdqngusanr@users.noreply.github.com>
2022-12-19 11:21:01 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 17.04 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.kt
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.kt
@@ -1,7 +1,3 @@
 class Solution {
-    fun solution(my_string: String): IntArray {
-        var answer: IntArray = intArrayOf()
-        answer = my_string.filter(Char::isDigit).chunked(1).sorted().toList().map{it -> it.toInt()}.toIntArray()
-        return answer
-    }
+    fun solution(my_string: String) = my_string.filter{ it.isDigit() }.map{ it.toString().toInt() }.sorted()
 }
