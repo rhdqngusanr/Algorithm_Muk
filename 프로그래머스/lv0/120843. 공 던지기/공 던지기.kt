eec3a13b0805aa71602970818e09fb944011aac1
rhdqngusanr <59686942+rhdqngusanr@users.noreply.github.com>
2022-12-27 17:37:43 +0900

    [level 0] Title: 공 던지기, Time: 0.01 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.kt
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.kt
@@ -1,14 +1,3 @@
 class Solution {
-    fun solution(numbers: IntArray, k: Int): Int {
-        var answer = numbers
-        var num = answer.size
-        println("$num")
-        while(num <= k*2){
-            answer = answer.plus(numbers)
-            num= answer.size
-            println("$num")
-
-        }
-        return answer[k*2-2]
-    }
+    fun solution(numbers: IntArray, k: Int) = numbers[ (k * 2 - 2) % numbers.size]
 }
