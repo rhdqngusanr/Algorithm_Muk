ffd40e654054c57793f6c2b61dd586b61c558109
rhdqngusanr <59686942+rhdqngusanr@users.noreply.github.com>
2023-01-03 17:31:13 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 59.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.kt
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun solution(hp: Int): Int {
-        var answer: Int = 0
-        answer += hp/5
-        var t = hp%5
-        answer += t/3
-        answer += t%3
-        return answer
-    }
+    fun solution(hp: Int) = hp / 5 + hp % 5 / 3 + hp % 5 % 3
 }
