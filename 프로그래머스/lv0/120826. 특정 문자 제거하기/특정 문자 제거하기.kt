49f04e10d5e63730a1643cf4aa300412536e68f9
rhdqngusanr <59686942+rhdqngusanr@users.noreply.github.com>
2023-01-10 11:16:01 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 10.00 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(my_string: String, letter: String) = my_string.split(letter,"").joinToString("")
+    fun solution(my_string: String, letter: String): String  = my_string.replace(letter, "")
 }
