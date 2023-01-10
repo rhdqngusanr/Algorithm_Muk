41a2a1f9c635770a64bbeb7d28f000cafd3589d7
rhdqngusanr <59686942+rhdqngusanr@users.noreply.github.com>
2023-01-10 11:02:41 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(num1: Int, num2: Int) = (num1.toFloat() / num2.toFloat() * 1000f).toInt()
+    fun solution(num1: Int, num2: Int) = (num1.toFloat() / num2 * 1000f).toInt()
 }
