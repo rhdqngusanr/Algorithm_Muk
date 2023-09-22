f0e77ffe0fd04b0eaa46b68d71c26080a36038fa
rhdqngusanr <59686942+rhdqngusanr@users.noreply.github.com>
2023-09-22 14:23:03 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 195.76 ms, Memory: 47.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    val s1 = readLine()!!
+    
+    val s2 = s1.toMutableList()
+    
+    for(i in s2.indices){
+        if(s2[i].isUpperCase()) {
+           
+            s2[i] = s2[i].toLowerCase()
+        }
+        else if(s2[i].isLowerCase()) {
+            
+            s2[i] = s2[i].toUpperCase()
+        }
+    }
+    
+    
+    print(s2.joinToString(""))
+}
